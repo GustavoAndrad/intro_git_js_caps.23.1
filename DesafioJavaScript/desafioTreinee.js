bb3b67725c6dc32afc84bf89aba6650fa6a6c8bb
Gustavo  Barros <gustavoandrade0125@gmail.com>
2023-05-13 01:04:47 -0300

    mudancas

---
+++ DesafioJavaScript/desafioTreinee.js
@@ -0,0 +1,19 @@
+const dadosEntrada = [
+    {nome: "João", idade: 17},
+    {nome: "Lucas", idade: 17},
+    {nome: "Geovana", idade: 17},
+    {nome: "Gustavo", idade: 17},
+    {nome: "Ana", idade: 17},
+    {nome: "Gabriela", idade: 17},
+    {nome: "Gabriel", idade: 17},
+    {nome: "Marcos", idade: 17},
+    {nome: "Lara", idade: 17},
+    {nome: "Julio", idade: 17}
+];
+
+function separaNomesLetraG(dadosEntrada){
+    let nomesComLetraG = dadosEntrada.filter(dadosEntrada => (dadosEntrada.nome[0]==='G'));;
+    return nomesComLetraG;
+}
+
+console.log("oi");
