392afb5e356b59dab1969a33e3e7bbc924ffb481
Gustavo  Barros <gustavoandrade0125@gmail.com>
2023-05-13 02:32:59 -0300

    Alterações Finais (git e javascript)
    última atualização para os desafios de git e javascript do ieee

--- DesafioJavaScript/desafioTreinee.js
+++ DesafioJavaScript/desafioTreinee.js
@@ -1,19 +1,34 @@
 const dadosEntrada = [
-    {nome: "João", idade: 17},
+    {nome: "João", idade: 10},
     {nome: "Lucas", idade: 17},
-    {nome: "Geovana", idade: 17},
+    {nome: "Geovana", idade: 10},
     {nome: "Gustavo", idade: 17},
-    {nome: "Ana", idade: 17},
-    {nome: "Gabriela", idade: 17},
-    {nome: "Gabriel", idade: 17},
-    {nome: "Marcos", idade: 17},
-    {nome: "Lara", idade: 17},
-    {nome: "Julio", idade: 17}
+    {nome: "Ana", idade: 56},
+    {nome: "Gabriela", idade: 13},
+    {nome: "Gabriel", idade: 4},
+    {nome: "Marcos", idade: 12},
+    {nome: "Lara", idade: 7},
+    {nome: "Julio", idade: 13}
 ];
+//Definição dos dados de entrada
 
-function separaNomesLetraG(dadosEntrada){
-    let nomesComLetraG = dadosEntrada.filter((dadosEntrada) => dadosEntrada.nome[0] == "G");
-    return nomesComLetraG;
+
+const lista_Itens_Nomes_Com_G = dadosEntrada.filter((dadosEntrada) => dadosEntrada.nome[0] == "G");
+// Array com os itens das pessoas com nomes que COMEÇAM com a letra G
+// Terá mais de uso no código
+
+function retornaNomesLetraG(){ // Utiliza lista_Itens_Nomes_Com_G e retorna os nomes das pessoas (letra G)
+    let nomesComLetraG = lista_Itens_Nomes_Com_G.map((dadosEntrada) => dadosEntrada.nome);
+    return nomesComLetraG.join(", ");
 }
 
-console.log(separaNomesLetraG(dadosEntrada));
+function calculaMediaIdades(){ // Utiliza lista_Itens_Nomes_Com_G e retorna a média das idades das pessoas (letra G)
+    let somaIdades = lista_Itens_Nomes_Com_G.reduce((a,b) => a + b.idade ,0);
+    let mediaIdades = somaIdades / lista_Itens_Nomes_Com_G.length;
+    return mediaIdades;
+}
+
+//Área de impressão
+console.log("====================RESOLUÇÃO====================");
+console.log("==> Pessoas com nome começado com a letra G: ", retornaNomesLetraG());
+console.log("==> Média geral das idades: ", calculaMediaIdades());
